a054230edb1c5b73074685c99481bc2f8577af66
Rabindra Raj Sah <53372486+Rabindra20@users.noreply.github.com>
2022-01-17 13:02:35 +0545

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 # provider "aws" {
 #   region     = "us-east-1"
-#   access_key = "ASIA52BEGI3BMIGT7URG"
-#   secret_key = "/m4hZscvoU9+4oMlYk9d36EOkpC12EGsGi9WV0Ic"
+#   access_key = ""
+#   secret_key = ""
 #   # for sso role in aws
-#   token = "IQoJb3JpZ2luX2VjEFUaCXVzLWVhc3QtMiJIMEYCIQD0h24Nt8Um7wu7J4IwU1lzp76Bk76ddtZ62KsYSG9h5QIhAK++5Khe/bBfbgsdcPtA5fGJ+rDKK8FGEkODHtq/5NrUKqIDCG4QABoMOTQ5MjYzNjgxMjE4Igz/ALyXq/qI6uJ5TcUq/wJZYn6K2/aei+omjMHQapXiVLSLdmfwWxD1uOSarejE6hCmIOuhDxzLorHvQ1FcFaeDlgsUdOwX1bBN/ythrHsj+IAmz7VWTXg+8QFEMwxG4WkZr/QFBqu89s6XO+o3DNa+fR60HPBT0ZwEVVgsdBuzyygobO5Zy3nNuo9CTrTJKRfrWp9Si9aBQY4VLjK/XiJYBbElRkoHzqlN84RTlr5LAC7R8qwKCHgBX/A8C4Y3uLvjMphy6q8V1o5QYygTcMdmg4xp5MSuSj5fkByTaDuJRer1DGzUAfsEdNAcvf4QwaTFJvhrua8DUwWqqvuYTc6ldxjJ5BnZXi7ET09NN1ods/uuJ2/trQUvCkkPfGuys67I5wNhPAVYn6BJ4WqYMEVGV7urlgioNoH/Y75dZa6nvNuqtPJYCaeONvgP1Ikg5IT7jK/xeQSFuA6SsGNWU9RA8OG3Uv2XGTnQA7fwFIt9VuAwfaOPB+adSemiV7gxfJZPF4NMOjsgA/IKn6NiTzCJ/IOPBjqlATEXN3Y3Yyf9q5d1u7/KeYhgd1wK0AOWqdiBZ88O17j99bRMFzLpLZMXpjbJICKJPdgjtT4Q2Zt9+ZXD44rDZHpltOXbBBUXHtFZrGPStDw1vKaUp/UhzH6OqwR3gSyOQi7jS0SSfz7SWuFp5r9UuzjeXNDljlLBpHfT/yqrD74btQ39rlX2UsSUOL+Fm8ogvqeSCty10lCkATYHmILxf3JYp6IfbQ=="
-# }
+#   token = ""
+#}
 module "vpc" {
   source = "./modules/vpc"
 
